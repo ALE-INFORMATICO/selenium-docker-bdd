35133ef2787410fd6804ad91f94120d799fba42b
acontrerass <acontrerass@ripley.com>
2021-01-22 19:53:59 -0300

    se agrega opcion de paralelismo en escenarios

--- src/test/java/com/runner/TestRunner.java
+++ src/test/java/com/runner/TestRunner.java
@@ -2,6 +2,7 @@ package com.runner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
         features = "src/test/resources/features",
@@ -14,5 +15,11 @@ import io.cucumber.testng.CucumberOptions;
                 "rerun:target/cucumber-reports/rerun-reports/rerun.txt"
         })
 public class TestRunner extends AbstractTestNGCucumberTests{
+
+        @Override
+        @DataProvider(parallel = true)
+        public Object[][] scenarios() {
+                return super.scenarios();
+        }
 
 }

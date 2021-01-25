aa4be8635a1405ac2ff4edfcf47a964101ce0241
acontrerass <acontrerass@ripley.com>
2021-01-24 21:58:28 -0300

    update healthcheck

--- healthcheck.sh
+++ healthcheck.sh
@@ -5,12 +5,12 @@ echo "Chequeando si el HUB está listo para ejecutar las pruebas"
 echo "IP del hub es -> $HUB_HOST"
 echo "........................................................."
 
-while [ "$(curl -s http://$HUB_HOST:4444/status | jq -r .value.nodes[0].availability)" != "UP" ]
+while [ "$(curl -s http://162.222.178.134:4444/status | jq -r .value.nodes[0].availability)" != "UP" ]
 do
         sleep 5
 done
 
-while [ "$(curl -s http://$HUB_HOST:4444/status | jq -r .value.nodes[1].availability)" != "UP" ]
+while [ "$(curl -s http://162.222.178.134:4444/status | jq -r .value.nodes[1].availability)" != "UP" ]
 do
         sleep 5
 done

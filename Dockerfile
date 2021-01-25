cc7f9146a58dbda2a2d40ae9e8e833782f77e3d8
acontrerass <acontrerass@ripley.com>
2021-01-24 22:55:44 -0300

    update dockerfile and healthcheck

--- Dockerfile
+++ Dockerfile
@@ -11,8 +11,6 @@ ADD target/selenium-docker-bdd-tests.jar selenium-docker-bdd-tests.jar
 ADD target/classes/ classes/
 ADD target/test-classes/ test-classes/
 ADD target/libs/ libs/
-ADD src/test/resources/features features/
-ADD src/test/java/com/test/stepdefs/ stepdefs/
 ADD healthcheck.sh healthcheck.sh
 
 ENTRYPOINT sh healthcheck.sh
--- healthcheck.sh
+++ healthcheck.sh
@@ -5,15 +5,10 @@ echo "Chequeando si el HUB está listo para ejecutar las pruebas"
 echo "IP del hub es -> $HUB_HOST"
 echo "........................................................."
 
-while [ "$(curl -s http://localhost:4444/status | jq -r .value.nodes[0].availability)" != "UP" ]
+while [ "$(curl -s http://162.222.178.134:4444/status | jq -r .value.nodes[0].availability)" != "UP" ]
 do
-        sleep 5
-done
-
-while [ "$(curl -s http://localhost:4444/status | jq -r .value.nodes[1].availability)" != "UP" ]
-do
-        sleep 5
+        sleep 10
 done
 
 # start the java command
-java -cp selenium-docker-bdd.jar:selenium-docker-bdd-tests.jar:classes/:test-classes/:libs/* io.cucumber.core.cli.Main features/ --glue stepdefs/
+java -cp selenium-docker-bdd.jar:selenium-docker-bdd-tests.jar:classes/:test-classes/:libs/* -DHUB_HOST=162.222.178.134 io.cucumber.core.cli.Main

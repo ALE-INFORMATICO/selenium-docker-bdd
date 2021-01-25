ab12c0d57d3181d93930ee0207d9c52c530a09f3
acontrerass <acontrerass@ripley.com>
2021-01-24 22:47:31 -0300

    add features

--- healthcheck.sh
+++ healthcheck.sh
@@ -5,15 +5,15 @@ echo "Chequeando si el HUB está listo para ejecutar las pruebas"
 echo "IP del hub es -> $HUB_HOST"
 echo "........................................................."
 
-while [ "$(curl -s http://162.222.178.134:4444/status | jq -r .value.nodes[0].availability)" != "UP" ]
+while [ "$(curl -s http://localhost:4444/status | jq -r .value.nodes[0].availability)" != "UP" ]
 do
         sleep 5
 done
 
-while [ "$(curl -s http://162.222.178.134:4444/status | jq -r .value.nodes[1].availability)" != "UP" ]
+while [ "$(curl -s http://localhost:4444/status | jq -r .value.nodes[1].availability)" != "UP" ]
 do
         sleep 5
 done
 
 # start the java command
-java -cp selenium-docker-bdd.jar:selenium-docker-bdd-tests.jar:classes/:test-classes/:libs/* io.cucumber.core.cli.Main features/ stepdefs/
+java -cp selenium-docker-bdd.jar:selenium-docker-bdd-tests.jar:classes/:test-classes/:libs/* io.cucumber.core.cli.Main features/ --glue stepdefs/

c8ea3f24d5884628f9ea4bf6f13ee4386e4a3a97
acontrerass <acontrerass@ripley.com>
2021-01-24 23:18:58 -0300

    update healthcheck for receive parameter $HUB_HOST

--- healthcheck.sh
+++ healthcheck.sh
@@ -5,10 +5,10 @@ echo "Chequeando si el HUB está listo para ejecutar las pruebas"
 echo "IP del hub es -> $HUB_HOST"
 echo "........................................................."
 
-while [ "$(curl -s http://162.222.178.134:4444/status | jq -r .value.nodes[0].availability)" != "UP" ]
+while [ "$(curl -s http://$HUB_HOST:4444/status | jq -r .value.nodes[0].availability)" != "UP" ]
 do
         sleep 10
 done
 
 # start the java command
-java -cp selenium-docker-bdd.jar:selenium-docker-bdd-tests.jar:classes/:test-classes/:libs/* -DHUB_HOST=162.222.178.134 io.cucumber.core.cli.Main
+java -cp selenium-docker-bdd.jar:selenium-docker-bdd-tests.jar:classes/:test-classes/:libs/* -DHUB_HOST=$HUB_HOST io.cucumber.core.cli.Main
